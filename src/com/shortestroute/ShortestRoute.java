92f36d864f594648c03b75479b4ccb0c501d28ec
maario <jkivuo@utu.fi>
2014-09-29 20:48:53 +0300

    optimized algorithms changing data structures

--- src/com/shortestroute/ShortestRoute.java
+++ src/com/shortestroute/ShortestRoute.java
@@ -4,121 +4,123 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.PriorityQueue;
-import java.util.Set;
 
 public class ShortestRoute {
 	private int[][] matrix;
 	private int matrixSize;
-	private Set<Integer> handled;
+	private HashMap<Integer,Vertex> handled;
 	private PriorityQueue<Vertex> unHandled;
-	private static int[] distances;
-	private int startVertex;
-	private int destinationVertex;
+	private int startVertexId;
+	private int destinationVertexId;
 	private ArrayList<Integer> shortestPath;
-	private HashMap<Integer,Integer> reverseSteps;
+	private int[] distances;
 	
 	
 	public ShortestRoute(int[][] matrix) {
 		this.matrix = matrix;
-		handled = new HashSet<Integer>();
+		handled = new HashMap<Integer,Vertex>();
 		unHandled = new VertexPriorityQueue<Vertex>(matrix.length, new Vertex());
 		matrixSize = matrix.length;
-		distances = new int[matrixSize];
-		reverseSteps = new HashMap<Integer,Integer>();
 	}
-	
 	
 	protected int getSizeOfMatrix() {
 		return this.matrixSize;
 	}
 	
-	
-	protected int getDistanceToVertex(int vertex) {
-		return distances[vertex - 1];
+	protected int getDistanceToVertex(int vertexId) {
+		if(handled.get(vertexId) != null){
+			return handled.get(vertexId).costFromStart;
+		}
+		return distances[vertexId - 1];
 	}
-	
 	
 	protected ArrayList<Integer> getPath() {
 		return this.shortestPath;
 	}
 	
-	
 	protected void shortestPathBetweenNodes(int vertexA, int vertexB) {		
-		startVertex = vertexA;
-		destinationVertex = vertexB;
+		startVertexId = vertexA;
+		destinationVertexId = vertexB;
+		distances = new int[matrixSize];
 		
-		for (int i = 0; i < matrixSize; i++)
+		for(int i = 0; i<matrixSize; i++)
 			distances[i] = Integer.MAX_VALUE;
 		
-		unHandled.add(new Vertex(startVertex, 0));
-		distances[startVertex - 1] = 0;
+		distances[startVertexId - 1] = 0;
+		unHandled.add(new Vertex(startVertexId, 0));
 		calculateShortestDistanceFromVertex();
 		shortestPath = findShortestRoutePath();
 	}
-	
-	private ArrayList<Integer> findShortestRoutePath() {
-		ArrayList<Integer> shortestPath = new ArrayList<>();
-		shortestPath = buildPath(destinationVertex);
-		return shortestPath;
-	}
-	
-	private ArrayList<Integer> buildPath(int vertex) {
-		ArrayList<Integer> path = new ArrayList<>();
-		if(vertex == startVertex){
-			path.add(startVertex);
-		}
-		else {
-			path = buildPath(reverseSteps.get(vertex));
-			path.add(vertex);
-		}
-		return path;
-	}
-	
+		
 	private void calculateShortestDistanceFromVertex() {
 		while (!unHandled.isEmpty()) {
-			int nextVertex = getVertexWithMinDistance();
-			if (nextVertex == destinationVertex)
+			Vertex nextVertex = getVertexWithMinDistance();
+			handled.put(nextVertex.id,nextVertex);
+			if (nextVertex.id == destinationVertexId)
 				break;
-			handled.add(nextVertex);
 			evaluateNeighbourVertexes(nextVertex);
 		}
 	}
 	
 	
-	private int getVertexWithMinDistance() {
-		return unHandled.remove().id;
+	private Vertex getVertexWithMinDistance() {
+		return unHandled.remove();
 	}
 	
 	
-	private void evaluateNeighbourVertexes(int vertex) {
+	private void evaluateNeighbourVertexes(Vertex vertex) {
 		for (int i = 1; i <= matrixSize; i++) {
-			if (!handled.contains(i)) {
-				if (matrix[vertex - 1][i - 1] != -1) {
-					int newDistance = distances[vertex - 1] + matrix[vertex - 1][i - 1];
+			if (handled.get(i) == null) {
+				if (matrix[vertex.id - 1][i - 1] != -1) {
+					int newDistance = distances[vertex.id - 1] + matrix[vertex.id - 1][i - 1];
 					if (newDistance < distances[i - 1]) {
 						distances[i - 1] = newDistance;
-						reverseSteps.put(i, vertex);
+						unHandled.add(new Vertex(i, distances[i - 1],vertex));
 					}
-					unHandled.add(new Vertex(i, distances[i - 1]));
 				}
 			}
 		}
 	}
 	
+	private ArrayList<Integer> findShortestRoutePath() {
+		ArrayList<Integer> shortestPath = new ArrayList<>();
+		Vertex destinationVertex = handled.get(destinationVertexId);
+		shortestPath = buildPath(destinationVertex);
+		return shortestPath;
+	}
+	
+	private ArrayList<Integer> buildPath(Vertex vertex) {
+		ArrayList<Integer> path = new ArrayList<>();
+		if(vertex.id == startVertexId){
+			path.add(vertex.id);
+		}
+		else {
+			path = buildPath(vertex.previousVertex);
+			path.add(vertex.id);
+		}
+		return path;
+	}
 	
 	private static class Vertex implements Comparator<Vertex> {
 		private int id;
 		private int cost;
+		private int costFromStart;
+		private Vertex previousVertex;
 		
 		public Vertex() {}
 		
 		public Vertex(int id, int cost) {
+			this(id,cost,null);
+		}
+		
+		public Vertex(int id, int cost, Vertex vertex) {
 			this.id = id;
 			this.cost = cost;
+			this.costFromStart = cost;
+			this.previousVertex = vertex;
 		}
-	   
+
 		@Override
 	    public int compare(Vertex vertexA, Vertex vertexB) {
 			return (vertexA.cost < vertexB.cost) ? -1 : (vertexA.cost > vertexB.cost) ? 1 : 0;
@@ -166,9 +168,9 @@ public class ShortestRoute {
 			new GraphToMatrix("graphs/graph1.txt").printMatrix();
 			System.out.println('\n');
 			
-			ArrayList<Integer> longestAndShortestPath = new ArrayList<>();
-			Integer cost = 0;
-			long start = System.currentTimeMillis();
+			//ArrayList<Integer> longestAndShortestPath = new ArrayList<>();
+			//Integer cost = 0;
+			//long start = System.currentTimeMillis();
 			/*for (int i = 0; i < 128; i++) {
 				for (int j = 0; j < 128; j++) {
 					if (i != j) {
@@ -190,11 +192,11 @@ public class ShortestRoute {
 			
 			sr = new ShortestRoute(new GraphToMatrix("graphs/graph1.txt").getMatrix());
 			sr.shortestPathBetweenNodes(2, 1);
-			System.out.printf("\nShortest distance from %s to %s is %s\n", 2, 81, distances[80]);			
-			System.out.printf("Shortest path is %s\n",sr.shortestPath.toString());
+			//System.out.printf("\nShortest distance from %s to %s is %s\n", 2, 81, distances[80]);			
+			//System.out.printf("Shortest path is %s\n",sr.shortestPath.toString());
 			
-			System.out.printf("%s",sr.reverseSteps.toString());
-			System.out.println(System.currentTimeMillis() - start);
+			//System.out.printf("%s",sr.reverseSteps.toString());
+			//System.out.println(System.currentTimeMillis() - start);
 		} 
 		catch (FileNotFoundException e) {
 			System.out.println("File not found\n");
