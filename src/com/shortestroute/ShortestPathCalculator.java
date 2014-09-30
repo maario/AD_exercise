a55a3780075f497ebd0bfaae770a93475f3633f4
Marco Willgren <mhw1@hotmail.com>
2014-09-30 23:21:00 +0300

    Simplified run class and added validation checking for arguments

--- src/com/shortestroute/ShortestPathCalculator.java
+++ src/com/shortestroute/ShortestPathCalculator.java
@@ -2,8 +2,10 @@ package com.shortestroute;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ShortestPathCalculator {
+	private static final String NUMBER_REGEX = "\\d+";
 	private static final String HEAD_TEXT = "\n\n"
 			+ "\t====================================\n"
 			+ "\t||                                ||\n"
@@ -14,23 +16,29 @@ public class ShortestPathCalculator {
 	
 	public static void main(String... args) {
 		try {
-			long startTime = System.currentTimeMillis();
-			ShortestRoute sr = new ShortestRoute(new GraphToMatrix("graphs/graph1.txt").getMatrix());
-			ShortestRoute sr2 = new ShortestRoute(new GraphToMatrix("graphs/graph2.txt").getMatrix());
-			
-			System.out.println(HEAD_TEXT);
+			long startTime = System.currentTimeMillis();		
 			
 			if (args == null || args.length < 2) {
+				printHeader();
 				System.out.println("Searching the shortest path containing the most vertexes...\n");
-				System.out.println("\n... from graph1:");
+				System.out.println("\n... from graph1 (complete graph):");
 				findLongestShortestPath(new GraphToMatrix("graphs/graph1.txt").getMatrix());
-				System.out.println("\n... from graph2:");
+				System.out.println("\n... from graph2 (incomplete graph):");
 				findLongestShortestPath(new GraphToMatrix("graphs/graph2.txt").getMatrix());
 			}
-			else {
+			else if (args.length == 2 && isNumber(args[0]) && isNumber(args[1])) {
+				printHeader();
 				System.out.printf("Searching the shortest path between %s and %s...\n\n", args[0], args[1]);
-				shortestPathBetweenNodes(sr, sr2, args[0], args[1]);
+				System.out.println("\n... from graph1 (complete graph):");
+				shortestPathBetweenNodes(new GraphToMatrix("graphs/graph1.txt").getMatrix(), args[0], args[1]);
+				System.out.println("\n... from graph2 (incomplete graph):");
+				shortestPathBetweenNodes(new GraphToMatrix("graphs/graph2.txt").getMatrix(), args[0], args[1]);
 			}
+			else {
+				System.out.printf("ERROR! Invalid parameters given %s. Parameter validation:\nCorrect amount: 0 or 2\nFormat: numeric\n\n", Arrays.toString(args));
+				return;
+			}
+			
 			System.out.printf("\nTotal running time: %s (ms)\n\n\n", System.currentTimeMillis() - startTime);
 		} 
 		catch (FileNotFoundException e) {
@@ -42,27 +50,29 @@ public class ShortestPathCalculator {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	private static void printHeader() {
+		System.out.println(HEAD_TEXT);
+	}
 
 	
-	private static void shortestPathBetweenNodes(ShortestRoute sr, ShortestRoute sr2, String start, String destination) throws Exception {
+	private static boolean isNumber(String text) {
+		return text.matches(NUMBER_REGEX);
+	}
+	
+	
+	private static void shortestPathBetweenNodes(int[][] matrix, String start, String destination) throws Exception {
+		ShortestRoute sr = new ShortestRoute(matrix);
 		int maxSize = sr.getSizeOfMatrix();
-		if (maxSize > sr2.getSizeOfMatrix())
-			maxSize = sr2.getSizeOfMatrix();
-		
 		int startVertex = parseInt(start);
 		int destinationVertex = parseInt(destination);
 		if (startVertex < 1 || destinationVertex < 1 || startVertex > maxSize || destinationVertex > maxSize)
 			throw new Exception("Given arguments (" + start + ", " + destination + ") are invalid: must be 1 to " + maxSize + "\n");
 		
 		sr.shortestPathBetweenNodes(startVertex, destinationVertex);
-		System.out.println("\n... in graph1:");
 		System.out.printf("\nShortest distance from %s to %s is %s\n", startVertex, destinationVertex, sr.getDistanceToVertex(destinationVertex));			
 		System.out.printf("Route: %s",sr.getPath().toString());
-		System.out.println("\n");
-		sr2.shortestPathBetweenNodes(startVertex, destinationVertex);
-		System.out.println("\n... in graph2:");
-		System.out.printf("\nShortest distance from %s to %s is %s\n", startVertex, destinationVertex, sr2.getDistanceToVertex(destinationVertex));			
-		System.out.printf("Route: %s",sr2.getPath().toString());
 		System.out.println("\n");
 	}
 
